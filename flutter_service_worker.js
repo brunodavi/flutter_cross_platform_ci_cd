da148796853db720c5013ad986b74e0f12d12c09
brunodavi <brunodaviandrade2000@gmail.com>
2025-01-19 19:43:14 +0000

    Deploying to gh-pages from @ brunodavi/flutter_cross_platform_ci_cd@288555abab8c7b055ddfa05565fcd923ed78c7bc 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "/": "f5906b714cc9807a8dde6d3008e9f295",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "cfc9cb56a49e0ed2ac88a7b66069fda6",
+"flutter_bootstrap.js": "e78955a5040503f24c4fb586f176dd40",
 "main.dart.js": "bedc6c9c3811146fa74c8bfe7c9247ff"};
 // The application shell files that are downloaded before a service worker can
 // start.
